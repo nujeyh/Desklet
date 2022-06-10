fa25637df8078a36853d3408ed9b645dfce16501
gureumwoon <vfrvfr3221@gmail.com>
2022-06-10 19:59:06 +0900

    signup signin route 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" />
+      </Routes>
     </div>
   );
 }
