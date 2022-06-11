54879ce39b5b708e20dc716267eefe9404341c7e
gureumwoon <vfrvfr3221@gmail.com>
2022-06-11 10:39:53 +0900

    Login Route 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
