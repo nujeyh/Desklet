600608d84aa5286b271ed6fee65b5af29cbc10e3
gureumwoon <vfrvfr3221@gmail.com>
2022-06-11 16:43:10 +0900

    Route 추가 & 기본 스타일 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Upload from "./pages/Upload";
+import axios from "axios";
 
 function App() {
   return (
