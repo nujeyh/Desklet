fd0694a0e3533edc499a3bd8f8664adb33b33f3d
gureumwoon <vfrvfr3221@gmail.com>
2022-06-12 12:05:31 +0900

    redux redux-thunk 환경설정

--- frontend/src/redux/configStore.js
+++ frontend/src/redux/configStore.js
@@ -1,11 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
-import postReducer from "./modules/postSlice";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+// import postReducer from "./modules/postSlice";
+import user from "./modules/user";;
 
-const store = configureStore({
-  reducer: {
-    posts: postReducer,
-  },
-});
+const middlewares = [thunk];
+const rootReducer = combineReducers({ user })
+const enhancer = applyMiddleware(...middlewares);
+
+const store = createStore(rootReducer, enhancer);
 
 export default store;
 //
