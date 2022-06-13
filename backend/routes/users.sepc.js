e0048cd5e4332128cced1b7deda659b7415e7bf9
hyungguen <fnvkd5316@gmail.com>
2022-06-13 16:16:50 +0900

    change mongoDb address

--- backend/routes/users.sepc.js
+++ backend/routes/users.sepc.js
@@ -1,12 +1,29 @@
-const { isEmail } = require('./users.js');
+const users = require('./users.js');
 const app = require("../app.js");
-const supertest = require("supertest");
+const request = require("supertest");
 
 
-test('테스트가 성공하는 상황', () => {
+test('userId는 이메일 형식을 가져야한다. ID + @ + 도메인+ (.com, .net)', () => {
     expect(isEmail('이메일이 아니에요')).toEqual(false);
 });
 
 test('테스트가 실패하는 상황', () => {
     expect(isEmail('my-email@domain.com')).toEqual(true);
 });
+
+test('테스트가 성공하는 상황', () => {
+  expect(isEmail('이메일이 아니에요')).toEqual(false);
+});
+
+test('테스트가 실패하는 상황', () => {
+  expect(isEmail('my-email@domain.com')).toEqual(true);
+});
+
+test('테스트가 성공하는 상황', () => {
+  expect(isEmail('이메일이 아니에요')).toEqual(false);
+});
+
+test('테스트가 실패하는 상황', () => {
+  expect(isEmail('my-email@domain.com')).toEqual(true);
+});
+
