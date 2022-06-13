7376a3cdc9de63a9d447ed7237d019225250e77f
nujeyh <dza456@naver.com>
2022-06-13 09:26:07 +0900

    Main, Detail 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,27 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import Upload from "./pages/Upload";
+import Main from "./pages/Main";
+// import Login from "./pages/Login";
+// import Upload from "./pages/Upload";
 import axios from "axios";
+import { useEffect } from "react";
+import { getPostListDB } from "./redux/modules/post";
+import { useDispatch } from "react-redux";
+import Detail from "./pages/Detail";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPostListDB());
+  }, [dispatch]);
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/posts/:id" element={<Detail />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/upload" element={<Upload />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/upload" element={<Upload />} /> */}
       </Routes>
     </div>
   );
