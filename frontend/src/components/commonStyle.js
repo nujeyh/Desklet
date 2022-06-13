b9990e1deb8807951471b568b6717a8498aacd31
nujeyh <dza456@naver.com>
2022-06-13 09:26:27 +0900

    commonStyle 생성

---
+++ frontend/src/components/commonStyle.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const MainBody = styled.div`
+  width: 70%;
+  margin: 10px auto;
+  @media screen and (max-width: 700px) {
+    width: 95%;
+  }
+`;
