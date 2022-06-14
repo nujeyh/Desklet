792e6e390c812ecf21c00525dedbd6d4859ad0fa
playhuck <wngur89@gmail.com>
2022-06-14 11:22:09 +0900

    Date.now

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -9,8 +9,8 @@ const storage = multer.diskStorage({
   destination: function(req, file, cb) {
     cb(null, 'uploads/')
   },
-  filename: function(req, file, cb) {
-    cb(null, file.originalname);
+  filename: function(req, file, cb) {   
+    cb(null, Date.now().file.originalname);
   }
 });
 
