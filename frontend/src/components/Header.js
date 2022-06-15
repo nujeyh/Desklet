91119f17d85174d3dbb6a8e1bb8fa8ea193729ad
nujeyh <dza456@naver.com>
2022-06-15 20:23:40 +0900

    닉네임 불러오도록 수정

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { MainBody, Hr } from "../elements/commonStyle";
 import { MainBtn, SubBtn } from "../elements/Btn";
@@ -11,13 +11,13 @@ import { logoutDB } from "../redux/modules/user";
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [isLogin, setIsLogin] = useState(true);
+  const isLogin = useSelector((state) => state.user.is_login);
+  const nickName = useSelector((state) => state.user.user.nickName);
 
   // 로그아웃하기
   const onClickSignOut = () => {
     const confirm = window.confirm("로그아웃 하시겠어요?");
     if (confirm) {
-      console.log("로그아웃 클릭");
       dispatch(logoutDB());
     }
   };
@@ -29,11 +29,9 @@ const Header = () => {
           <div onClick={() => navigate("/")}>Desklet</div>
           {isLogin ? (
             <nav>
-              <span>{"nickname"}님</span>
+              <span>{nickName}님</span>
               <MainBtn onClick={() => navigate("/upload")}>글쓰기</MainBtn>
               <SubBtn onClick={onClickSignOut}>로그아웃</SubBtn>
-              <MainBtn onClick={() => navigate("/login")}>로그인</MainBtn>
-              <SubBtn onClick={() => navigate("/signup")}>회원가입</SubBtn>
             </nav>
           ) : (
             <nav>
@@ -53,10 +51,6 @@ const HeaderWrap = styled.div`
   flex-direction: row;
   justify-content: space-between;
   padding: 10px 0;
-
-  h2 {
-    cursor: pointer;
-  }
 
   nav {
     margin: auto 0;
