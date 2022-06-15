1e9c7569119babd47da430fd2b60e1fc1c097d8f
nujeyh <dza456@naver.com>
2022-06-15 14:26:55 +0900

    경로 수정

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-import { MainBody, Hr } from "./commonStyle";
+import { MainBody, Hr } from "../elements/commonStyle";
 import { MainBtn, SubBtn } from "../elements/Btn";
 import { logoutDB } from "../redux/modules/user";
 
